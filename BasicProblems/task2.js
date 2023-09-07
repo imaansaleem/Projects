ae12bb1f1f1f17bebada781a0cf53518418dcd86
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-07 15:33:17 +0500

    Add files via upload

---
+++ BasicProblems/task2.js
@@ -0,0 +1,4 @@
+let personName; //Declared a variable
+personName = prompt("Please enter your name!"); //Taking input of Person's name
+if (personName) console.log("Hello " + personName + "! It seems as you are solving javascript today!"); //displaying name if person has entered his name
+else console.log("Alas! You didn't write your name!") //if person has not entered his name 
