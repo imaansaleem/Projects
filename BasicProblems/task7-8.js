d19b9ea547baaee5a8adeb195fd7c9d4e76c7037
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-07 15:34:39 +0500

    Add files via upload

---
+++ BasicProblems/task7-8.js
@@ -0,0 +1,5 @@
+console.log("Result of 3+2+1+2 is: ", 3+2+1+2);
+console.log("Result of 16-3-1-4 is: ", 16-3-1-4);
+console.log("Result of 4*2 is: ", 4*2);
+console.log("Result of 16/2 is: ", 16/2);
+console.log("Result of ((16+8-4)/2)-2is: ", ((16+8-4)/2)-2);
