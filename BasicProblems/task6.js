09b36d0e1a3f6983e49ec71390ab81506ff0adab
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-07 15:34:27 +0500

    Add files via upload

---
+++ BasicProblems/task6.js
@@ -0,0 +1,5 @@
+let personName;
+personName = prompt("Please enter your full name!");
+if(personName){
+    console.log("Name with white space is:\n....\t"+personName+'\t....\nand the name without whitespace is '+personName);
+}
