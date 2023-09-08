02309c9bf06c6bbea842ce48bf1c9fa86c61817a
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-09 00:11:23 +0500

    Add files via upload

---
+++ BasicProblems/task22.js
@@ -0,0 +1,7 @@
+const mountains=["Everest","K2","Kanchenjunga","Lhotse","Makalu"];
+/*console.log(mountains[99999]);
+    As our array is smaller than tha specified index so it is giving an error "undefined".
+    To  remove this error we need to write that index that is less than the actual size of array
+*/
+
+console.log(mountains[1]);
