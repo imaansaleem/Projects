420449b0b204d152a1841da6040af6533fd084f0
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-09 00:10:51 +0500

    Add files via upload

---
+++ BasicProblems/task20.js
@@ -0,0 +1,10 @@
+const mountains=["Everest","K2","Kanchenjunga","Lhotse","Makalu"];
+const rivers=["Nile","Niger","Euphrates","Kolyma","	Ganges"];
+const countries=["Afghanistan","Albania","Algeria","Andorra","Bhutan"];
+const cities=["Lahore","karachi","Islamabad","Kasur","Multan"];
+const languages=["Spanish","Hindi","English","French","Chinese"];
+console.log(mountains);
+console.log(rivers);
+console.log(countries);
+console.log(cities);
+console.log(languages);
