0936f39cee6ba947d2b76bd50265679e62856ff9
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-09 00:11:04 +0500

    Add files via upload

---
+++ BasicProblems/task21.js
@@ -0,0 +1,8 @@
+let list={
+    mountains: ["Everest","K2","Kanchenjunga","Lhotse","Makalu"],
+    rivers: ["Nile","Niger","Euphrates","Kolyma","Ganges"],
+    countries: ["Afghanistan","Albania","Algeria","Andorra","Bhutan"],
+    cities: ["Lahore","karachi","Islamabad","Kasur","Multan"],
+    languages: ["Spanish","Hindi","English","French","Chinese"],
+};
+console.log(list);
