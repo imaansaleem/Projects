358d6f0dd8f9f1f7677485fd53c0cc5d023bfe91
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-10 00:47:55 +0500

    Add files via upload

---
+++ BasicProblems/task23.js
@@ -0,0 +1,24 @@
+let animal = "dog";
+console.log("Is dog == 'animal'? I predict True.");
+console.log(animal == "dog");
+console.log("Is 7 < 10? I predict True.");
+console.log(7<10);
+console.log("Is string '3' is greater than string '10'? I predict True.");
+console.log("3" > "10");
+console.log("Is 10 equal to 5 or 5 equal to 5? I predict True.");
+console.log(10==5 || 5==5);
+let weather="good";
+console.log("Is 10 less than or equal to 10? I predict True.");
+console.log(10<=10);
+
+
+console.log("Is sum of 2 and 3 equal to 6? I predict False.");
+console.log((2+3)==6);
+console.log("Is 2 equal to string 'Umair'? I predict False.");
+console.log(2 == "Umair");
+console.log("Is string 'dog' Equal to string 'cat'? I predict False.");
+console.log("dog"=="cat");
+console.log("Is string HAMZA is equal to string 'hamza'? I predict False.");
+console.log("HAMZA"=="hamza");
+console.log("Is 8 less than 10 along with 3 less than 1? I predict False.");
+console.log(8<10 && 3<1);
