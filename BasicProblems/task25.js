898a329679206e22726d982e506e4ba14140e7d5
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-10 00:48:19 +0500

    Add files via upload

---
+++ BasicProblems/task25.js
@@ -0,0 +1,8 @@
+let alien_color="green";
+function color(col){
+    if(alien_color=="green")
+        console.log("You just earned 5 points");
+}
+color(alien_color);
+alien_color="red";
+color(alien_color);
