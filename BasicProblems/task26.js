060584b184af4e3b95a018dbb5221375de9885de
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-10 00:48:32 +0500

    Add files via upload

---
+++ BasicProblems/task26.js
@@ -0,0 +1,10 @@
+let alien_color="green";
+function color(col){
+    if(alien_color=="green")
+        console.log("You just earned 5 points for shooting the alien.");
+    else
+        console.log("You just earned 10 points");
+}
+color(alien_color);
+alien_color="red";
+color(alien_color);
