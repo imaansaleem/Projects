114e899cc7544cd2dfa55a959524eb70bae4fdc8
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-12 14:44:55 +0500

    Add files via upload

---
+++ BasicProblems/task34.js
@@ -0,0 +1,9 @@
+const pizza=["Veggie","Cheese","Pepperoni","Margherita","Hawaiian"];
+pizza.forEach(n=>console.log(n));
+
+let i=0;
+while(i<pizza.length){
+    console.log("I like "+pizza[i]+" pizza");
+    i++;
+}
+console.log("Pizza is the only food worthy. I can eat it at anytime of the day and it will still taste perfect. I like to eat Cheese Pizza, Veggie Pizza, Pepperoni Pizza, Meat Pizza, Margherita Pizza, BBQ Chicken Pizza, Hawaiian Pizza, Buffalo Pizza.I really love pizza!");
