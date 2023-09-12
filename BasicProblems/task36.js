18c2ee41236f27e382cbdb6d0c28796a08e3a51b
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-12 14:45:38 +0500

    Add files via upload

---
+++ BasicProblems/task36.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message){
+    console.log("Shirt of size "+size+" has a message "+message+" written on it");
+}
+mess="'Whatever it is I didn't do it'";
+make_shirt(10,mess);
