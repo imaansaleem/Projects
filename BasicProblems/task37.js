7252410380bc82f5090fee6cbb0fc6f3a48779ef
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-12 14:45:54 +0500

    Add files via upload

---
+++ BasicProblems/task37.js
@@ -0,0 +1,11 @@
+function make_shirt(size=100, message="'I love JavaScript'"){
+    console.log("Shirt of size "+size+" has a message "+message+" written on it");
+}
+mess="'Whatever it is I didn't do it'";
+console.log("Large Shirt");
+make_shirt();
+console.log("Medium Shirt");
+make_shirt(50);
+console.log("Large Shirt");
+make_shirt(20,"'I Love Programming'");
+
