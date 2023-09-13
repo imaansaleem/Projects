2eca2798765d38fd49e4509acd6a9b714991d00a
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-13 21:21:19 +0500

    Add files via upload

---
+++ BasicProblems/task39.js
@@ -0,0 +1,12 @@
+function city_country(name_of_city, country){
+    let message=name_of_city+", "+country;
+    return message;
+}
+
+let pairs;
+pairs=city_country("Dubai", "United Arab Emirates");
+console.log(pairs);
+pairs=city_country("Lahore", "Pakistan");
+console.log(pairs)
+pairs=city_country("Bradford", "England");
+console.log(pairs)
