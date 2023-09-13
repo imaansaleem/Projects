958a66470d3b3c8d54825c56f31533cce02ffc33
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-13 21:26:23 +0500

    Add files via upload

---
+++ BasicProblems/task45.js
@@ -0,0 +1,9 @@
+function car(manufacturer,model_name,...arr){
+    let information={
+        man: manufacturer,
+        name: model_name,
+        arr[0]: arr[i+1],
+    };
+}
+
+car("Honda","civic","color","red","seating_capacity","4");
