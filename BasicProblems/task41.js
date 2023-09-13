ef1ef991f9fa3b051f170b4d7f118539dba49dff
Imaan Saleem <99469449+imaansaleem@users.noreply.github.com>
2023-09-13 21:22:21 +0500

    Add files via upload

---
+++ BasicProblems/task41.js
@@ -0,0 +1,6 @@
+const magician_names=["JAY MARSHALL","MAX MAVEN","David Blaine","Derren Brown"];
+function show_magicians(arr){
+    arr.forEach(n=>console.log(n));
+}
+show_magicians(magician_names);
+
